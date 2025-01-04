3b68d269ce4608b8e5f9519f5b3b94930cd802e1
pekanov <pekanov300@yandex.ru>
2025-01-04 16:50:22 +0300

    add new

---
+++ src/main/java/org/skillfactory/sf_shortlink/controller/UserNotificationController.java
@@ -0,0 +1,27 @@
+package org.skillfactory.sf_shortlink.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.skillfactory.sf_shortlink.service.UserNotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
+public class UserNotificationController {
+
+    private final UserNotificationService userNotificationService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserNotification(@PathVariable("userId") UUID userId) {
+        return ResponseEntity.ok(userNotificationService.getUserNotifications(userId));
+    }
+
+
+}
